39a6bc66ed023939f0c0a2bde610b24bfe2ab5f5
sfali <sali2@loyalty.com>
2017-11-16 08:28:47 -0500

    better way to print time

--- AlgorithmicToolbox/Assignment3/src/test/scala/DataGenerator.scala
+++ AlgorithmicToolbox/Assignment3/src/test/scala/DataGenerator.scala
@@ -31,8 +31,23 @@ trait DataGenerator {
 
   def printTimeElapsed(startTime: Long, description: String): Unit = {
     val endTime = System.nanoTime()
-    val seconds = TimeUnit.NANOSECONDS.toSeconds(endTime - startTime)
-    val millis = TimeUnit.NANOSECONDS.toMillis(endTime - startTime) % 1000
-    println(s"$description: $seconds seconds $millis ms")
+    val timeElapsed = endTime - startTime
+
+    var label = ""
+
+    var unit = TimeUnit.NANOSECONDS.toMillis(timeElapsed)
+    val millis = if (unit > 1000) unit % 1000 else unit
+    label = if (millis > 0) s"$millis ms" else label
+
+    unit = TimeUnit.NANOSECONDS.toSeconds(timeElapsed)
+    val seconds = if (unit > 60) unit % 60 else unit
+    label = if (seconds > 0) s"$seconds seconds " + label else label
+
+    unit = TimeUnit.NANOSECONDS.toMinutes(timeElapsed)
+    val minutes = if (unit > 60) unit % 60 else unit
+    label = if (minutes > 0) s"$minutes minutes " + label else label
+    label = if(label.isEmpty) s"$timeElapsed ns" else label
+
+    println(s"$description: $label")
   }
 }
