ce4aee54cabed01940d8491bdbfe8247fa9c3fce
sfali23 <f.syed.ali@gmail.com>
2017-11-19 10:29:24 -0500

    tree height implementation and test

--- DataStructures/Assignment1/src/main/scala/TreeHeight.scala
+++ DataStructures/Assignment1/src/main/scala/TreeHeight.scala
@@ -1,6 +1,59 @@
-/**
-  * @author sali
-  */
-object TreeHeight {
+import scala.collection.mutable
+import scala.io.StdIn
 
+trait TreeHeightWork {
+
+  def computeHeight(root: Node): Int = {
+    var height = Int.MinValue
+    val queue = mutable.Queue[(Node, Int)]()
+    queue.enqueue((root, 1))
+    while (queue.nonEmpty) {
+      val (current, level) = queue.dequeue()
+      if (current.hasChildren)
+        current.children.foreach(node => queue.enqueue((node, level + 1)))
+      else height = height.max(level)
+    }
+    height
+  }
+
+  def readTree(n: Int, input: String): Node = {
+    val nodes = Array.ofDim[Node](n)
+    for (i <- nodes.indices) nodes(i) = new Node(i)
+
+    var root: Node = new Node(-1)
+    val values = input.split(" ")
+    for (i <- values.indices) {
+      val parentIndex = values(i).toInt
+      if (parentIndex >= 0) nodes(i).parent = nodes(parentIndex)
+      else root = nodes(i)
+    }
+    root
+  }
+}
+
+class Node(val key: Int) {
+  private var _parent: Node = _
+  private var _children: List[Node] = List()
+
+  def parent: Node = _parent
+  def parent_=(parent: Node): Unit = {
+    _parent = parent
+    parent.addChild(this)
+  }
+
+  def addChild(node: Node): Unit = _children = node :: _children
+
+  def children: List[Node] = _children
+
+  def hasChildren: Boolean = _children.nonEmpty
+
+  override def toString: String = key.toString
+}
+
+object TreeHeight extends App with TreeHeightWork {
+  new Thread(null, () => {
+    val n = StdIn.readInt()
+    val input = StdIn.readLine()
+    println(computeHeight(readTree(n, input)))
+  }, "1", 1 << 26).start()
 }
---
+++ DataStructures/Assignment1/src/test/scala/TreeHeightTest.scala
@@ -0,0 +1,45 @@
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+
+/**
+  * @author sali
+  */
+@RunWith(classOf[JUnitRunner])
+class TreeHeightTest
+    extends FunSuite
+    with TreeHeightWork
+    with BeforeAndAfterAll
+    with TestHelper {
+
+  private var startTime: Long = 0L
+
+  override protected def beforeAll(): Unit = {
+    startTime = System.nanoTime()
+  }
+
+  override protected def afterAll(): Unit = {
+    printTimeElapsed(startTime, "time to run all tests")
+  }
+
+  test("TreeHeight: Test case #1") {
+    val root = readTree(5, "4 -1 4 1 1")
+    assert(computeHeight(root) === 3)
+  }
+
+  test("TreeHeight: Test case #2") {
+    val root = readTree(5, "-1 0 4 0 3")
+    assert(computeHeight(root) === 4)
+  }
+
+  test("TreeHeight: read pre-defined tests") {
+    for (i <- 1 to 24) {
+      val fileNamePrefix = f"$i%02d"
+      val lines = readLines(s"/tree-height/$fileNamePrefix")
+      val n = lines.head.toInt
+      val input = lines.last
+      val output = readLines(s"/tree-height/$fileNamePrefix.a").head.toInt
+      assert(computeHeight(readTree(n, input)) === output)
+    }
+  }
+}
