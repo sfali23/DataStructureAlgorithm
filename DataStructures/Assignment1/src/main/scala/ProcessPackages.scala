2a884cd37cafeb7358bf7966a46f07f08c29a140
sfali23 <f.syed.ali@gmail.com>
2017-11-19 11:07:29 -0500

    initial draft of ProcessPackages

--- DataStructures/Assignment1/src/main/scala/ProcessPackages.scala
+++ DataStructures/Assignment1/src/main/scala/ProcessPackages.scala
@@ -1,6 +1,19 @@
-/**
-  * @author sali
-  */
-object ProcessPackages {
+trait ProcessPackagesWork {
+
+}
+
+case class Request(arrivalTime: Int, processTime: Int)
+
+case class Response(dropped: Boolean, startTime: Int)
+
+class Buffer(size: Int){
+  private var finishTime = List[Int]()
+
+  def process(request: Request): Response = {
+    Response(dropped = false, startTime = -1)
+  }
+}
+
+object ProcessPackages extends App with ProcessPackagesWork {
 
 }
