24c529878d07e3d817ff6ca06a33c7651f1028ee
sfali23 <f.syed.ali@gmail.com>
2017-11-20 17:25:02 -0500

    Process packages code and test case

--- DataStructures/Assignment1/src/main/scala/ProcessPackages.scala
+++ DataStructures/Assignment1/src/main/scala/ProcessPackages.scala
@@ -1,19 +1,60 @@
+import java.util.Scanner
+
+import scala.collection.mutable.ListBuffer
+
 trait ProcessPackagesWork {
 
+  def initBuffer(scanner: Scanner): Buffer = new Buffer(scanner.nextInt())
+
+  def readRequests(scanner: Scanner): List[Request] = {
+    val numOfRequests = scanner.nextInt()
+    val requests = ListBuffer[Request]()
+    for (_ <- 0 until numOfRequests)
+      requests += Request(scanner.nextInt(), scanner.nextInt())
+    requests.toList
+  }
+
+  def processRequests(buffer: Buffer,
+                      requests: List[Request]): List[Response] =
+    for {
+      request <- requests
+      response = buffer.process(request)
+    } yield response
 }
 
 case class Request(arrivalTime: Int, processTime: Int)
 
-case class Response(dropped: Boolean, startTime: Int)
+case class Response(startTime: Int = -1)
 
-class Buffer(size: Int){
-  private var finishTime = List[Int]()
+class Buffer(size: Int) {
+  private var buffer = List[Int]()
 
   def process(request: Request): Response = {
-    Response(dropped = false, startTime = -1)
+    val currentTime = request.arrivalTime
+    val processTime = request.processTime
+
+    if (buffer.nonEmpty && currentTime >= buffer.head) {
+      // if the front most processor is already finished
+      buffer = buffer.tail
+    }
+
+    if (buffer.isEmpty) {
+      val finishTime = processTime + currentTime
+      buffer = buffer :+ finishTime
+      Response(currentTime)
+    } else {
+      if (buffer.length < size) {
+        // we still have space in buffer, queue this request
+        val last = buffer.last
+        val finishTime = processTime + last
+        buffer = buffer :+ finishTime
+        Response(last)
+      } else {
+        // buffer is full, drop the packet
+        Response()
+      }
+    }
   }
 }
 
-object ProcessPackages extends App with ProcessPackagesWork {
-
-}
+object ProcessPackages extends App with ProcessPackagesWork {}
---
+++ DataStructures/Assignment1/src/test/scala/ProcessPackagesTest.scala
@@ -0,0 +1,47 @@
+import java.io.{BufferedInputStream, FileInputStream}
+import java.nio.file.Paths
+import java.util.Scanner
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+
+/**
+  * @author sali
+  */
+@RunWith(classOf[JUnitRunner])
+class ProcessPackagesTest
+    extends FunSuite
+    with BeforeAndAfterAll
+    with ProcessPackagesWork
+    with TestHelper {
+
+  private var startTime: Long = 0L
+
+  override protected def beforeAll(): Unit = {
+    startTime = System.nanoTime()
+  }
+
+  override protected def afterAll(): Unit = {
+    printTimeElapsed(startTime, "time to run all tests")
+  }
+
+  test("ProcessPackages: Test case #1") {
+    val start = 1
+    val end = 24
+    for (i <- start to end) {
+      val fileNamePrefix = f"$i%02d"
+      val path = Paths
+        .get(getClass.getResource(s"/process-packages/$fileNamePrefix").toURI)
+        .toAbsolutePath
+      System.setIn(new BufferedInputStream(new FileInputStream(path.toFile)))
+      val scanner = new Scanner(System.in)
+      val buffer = initBuffer(scanner)
+      val requests = readRequests(scanner)
+      val responses = processRequests(buffer, requests).map(_.startTime)
+      val output =
+        readLines(s"/process-packages/$fileNamePrefix.a").map(_.toInt)
+      assert(responses === output, s"'Failed test $fileNamePrefix'")
+    }
+  }
+}
