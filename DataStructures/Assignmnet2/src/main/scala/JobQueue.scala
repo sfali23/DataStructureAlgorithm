fec0ca17eca4019ffea25d123fac4501176e959b
sfali23 <f.syed.ali@gmail.com>
2017-11-27 07:45:57 -0500

    implementation of heap and tests

--- DataStructures/Assignmnet2/src/main/scala/JobQueue.scala
+++ DataStructures/Assignmnet2/src/main/scala/JobQueue.scala
@@ -1,4 +1,63 @@
-class JobQueueHeap() {}
+class JobQueueHeap(maxSize: Int) {
+
+  private var size = -1
+  private val storage = Array.ofDim[Int](maxSize)
+
+  def moveUp(i: Int): Unit = {
+    var index = i
+    while (index >= 0 && storage(index) < storage(parent(index))) {
+      val parentIndex = parent(index)
+      val tmp = storage(parentIndex)
+      storage(parentIndex) = storage(index)
+      storage(index) = tmp
+      index = parentIndex
+    }
+  }
+
+  def moveDown(i: Int): Unit = {
+    var minIndex = i
+    val l = leftChild(i)
+    minIndex = if (l <= size && storage(l) < storage(minIndex)) l else minIndex
+
+    val r = rightChild(i)
+    minIndex = if (r <= size && storage(r) < storage(minIndex)) r else minIndex
+
+    if (i != minIndex) {
+      val tmp = storage(i)
+      storage(i) = storage(minIndex)
+      storage(minIndex) = tmp
+      moveDown(minIndex)
+    }
+  }
+
+  def insert(value: Int): Unit =
+    if (size < maxSize) {
+      size += 1
+      storage(size) = value
+      moveUp(size)
+    }
+
+  def extractMin: Int = {
+    val result = storage(0)
+    storage(0) = storage(size)
+    size -= 1
+    moveDown(0)
+    result
+  }
+
+  def heap: Array[Int] = storage.take(size + 1)
+
+  def print(): Unit = println(heap.mkString("[", ", ", "]"))
+
+  private def parent(i: Int): Int = (i - 1) / 2
+  private def leftChild(i: Int) = 2 * i + 1
+  private def rightChild(i: Int) = 2 * i + 2
+
+}
+
+object JobQueueHeap {
+  def apply(maxSize: Int): JobQueueHeap = new JobQueueHeap(maxSize)
+}
 
 trait JobQueueWork {}
 
--- DataStructures/Assignmnet2/src/test/scala/JobQueueTest.scala
+++ DataStructures/Assignmnet2/src/test/scala/JobQueueTest.scala
@@ -6,4 +6,70 @@ import org.scalatest.junit.JUnitRunner
   * @author sali
   */
 @RunWith(classOf[JUnitRunner])
-class JobQueueTest extends FunSuite with BeforeAndAfterAll with JobQueueWork with TestHelper {}
+class JobQueueTest extends FunSuite with BeforeAndAfterAll with JobQueueWork with TestHelper {
+
+  test("build heap") {
+    val heap = JobQueueHeap(13)
+
+    heap.insert(42)
+    heap.insert(29)
+    heap.insert(18)
+    heap.insert(14)
+    heap.insert(7)
+    heap.insert(18)
+    heap.insert(12)
+    heap.insert(11)
+    heap.insert(13)
+    assert(heap.heap === Array(7, 11, 12, 13, 18, 29, 18, 42, 14))
+  }
+
+  test("extract min") {
+    val heap = JobQueueHeap(13)
+
+    heap.insert(42)
+    heap.insert(29)
+    heap.insert(18)
+    heap.insert(14)
+    heap.insert(7)
+    heap.insert(18)
+    heap.insert(12)
+    heap.insert(11)
+    heap.insert(13)
+
+    var min = heap.extractMin
+    assert(min === 7)
+    assert(heap.heap === Array(11, 13, 12, 14, 18, 29, 18, 42))
+
+    min = heap.extractMin
+    assert(min === 11)
+    assert(heap.heap === Array(12, 13, 18, 14, 18, 29, 42))
+
+    min = heap.extractMin
+    assert(min === 12)
+    assert(heap.heap === Array(13, 14, 18, 42, 18, 29))
+
+    min = heap.extractMin
+    assert(min === 13)
+    assert(heap.heap === Array(14, 18, 18, 42, 29))
+
+    min = heap.extractMin
+    assert(min === 14)
+    assert(heap.heap === Array(18, 29, 18, 42))
+
+    min = heap.extractMin
+    assert(min === 18)
+    assert(heap.heap === Array(18, 29, 42))
+
+    min = heap.extractMin
+    assert(min === 18)
+    assert(heap.heap === Array(29, 42))
+
+    min = heap.extractMin
+    assert(min === 29)
+    assert(heap.heap === Array(42))
+
+    min = heap.extractMin
+    assert(min === 42)
+    assert(heap.heap === Array())
+  }
+}
