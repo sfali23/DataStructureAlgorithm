1a72714ddf56e74a0c4bb52847575fe4bda410f1
sfali23 <f.syed.ali@gmail.com>
2017-11-27 07:45:19 -0500

    remove test

--- DataStructures/Assignmnet2/src/test/scala/BuildHeapTest.scala
+++ DataStructures/Assignmnet2/src/test/scala/BuildHeapTest.scala
@@ -1,77 +1,12 @@
 import org.junit.runner.RunWith
-import org.scalatest.{ BeforeAndAfterAll, FunSuite }
 import org.scalatest.junit.JUnitRunner
+import org.scalatest.{ BeforeAndAfterAll, FunSuite }
 
 /**
   * @author sali
   */
 @RunWith(classOf[JUnitRunner])
 class BuildHeapTest extends FunSuite with BeforeAndAfterAll with BuildHeapWork with TestHelper {
-
-  /*test("build heap") {
-    val heap = Heap(13)
-
-    heap.insert(42)
-    heap.insert(29)
-    heap.insert(18)
-    heap.insert(14)
-    heap.insert(7)
-    heap.insert(18)
-    heap.insert(12)
-    heap.insert(11)
-    heap.insert(13)
-    assert(heap.heap === Array(7, 11, 12, 13, 18, 29, 18, 42, 14))
-  }*/
-
-  /*test("extract min"){
-    val heap = Heap(13)
-
-    heap.insert(42)
-    heap.insert(29)
-    heap.insert(18)
-    heap.insert(14)
-    heap.insert(7)
-    heap.insert(18)
-    heap.insert(12)
-    heap.insert(11)
-    heap.insert(13)
-
-    var min = heap.extractMin
-    assert(min === 7)
-    assert(heap.heap === Array(11, 13, 12, 14, 18, 29, 18, 42))
-
-    min = heap.extractMin
-    assert(min === 11)
-    assert(heap.heap === Array(12, 13, 18, 14, 18, 29, 42))
-
-    min = heap.extractMin
-    assert(min === 12)
-    assert(heap.heap === Array(13, 14, 18, 42, 18, 29))
-
-    min = heap.extractMin
-    assert(min === 13)
-    assert(heap.heap === Array(14, 18, 18, 42, 29))
-
-    min = heap.extractMin
-    assert(min === 14)
-    assert(heap.heap === Array(18, 29, 18, 42))
-
-    min = heap.extractMin
-    assert(min === 18)
-    assert(heap.heap === Array(18, 29, 42))
-
-    min = heap.extractMin
-    assert(min === 18)
-    assert(heap.heap === Array(29, 42))
-
-    min = heap.extractMin
-    assert(min === 29)
-    assert(heap.heap === Array(42))
-
-    min = heap.extractMin
-    assert(min === 42)
-    assert(heap.heap === Array())
-  }*/
 
   test("BuildHeap: Test case #1") {
     val swaps = generateSwaps(Array(5, 4, 3, 2, 1))
