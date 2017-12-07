02bdaa65a673a136a41aa3fdf1e4cc85a0b4c7a0
sfali23 <f.syed.ali@gmail.com>
2017-12-07 07:39:27 -0500

    naive implementation of phone book to generate tests

--- DataStructures/Assignment3/src/main/java/NaivePhoneBook.java
+++ DataStructures/Assignment3/src/main/java/NaivePhoneBook.java
@@ -1,36 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class NaivePhoneBook {
 
-    private FastScanner in = new FastScanner();
     // Keep list of all existing (i.e. not deleted yet) contacts.
     private List<Contact> contacts = new ArrayList<>();
+    private List<String> responses = new ArrayList<>();
 
-    public static void main(String[] args) {
-        new NaivePhoneBook().processQueries();
-    }
-
-    private Query readQuery() {
-        String type = in.next();
-        int number = in.nextInt();
+    private Query readQuery(Scanner scanner) {
+        String type = scanner.next();
+        int number = scanner.nextInt();
         if (type.equals("add")) {
-            String name = in.next();
+            String name = scanner.next();
             return new Query(type, name, number);
         } else {
             return new Query(type, number);
         }
     }
-
-    private void writeResponse(String response) {
-        System.out.println(response);
-    }
-
 
     private void processQuery(Query query) {
         switch (query.type) {
@@ -62,15 +50,16 @@ public class NaivePhoneBook {
                         response = contact.name;
                         break;
                     }
-                writeResponse(response);
+                responses.add(response);
                 break;
         }
     }
 
-    public void processQueries() {
-        int queryCount = in.nextInt();
+    public List<String> processQueries(Scanner scanner) {
+        int queryCount = scanner.nextInt();
         for (int i = 0; i < queryCount; ++i)
-            processQuery(readQuery());
+            processQuery(readQuery(scanner));
+        return responses;
     }
 
     static class Contact {
@@ -97,30 +86,6 @@ public class NaivePhoneBook {
         public Query(String type, int number) {
             this.type = type;
             this.number = number;
-        }
-    }
-
-    class FastScanner {
-        BufferedReader br;
-        StringTokenizer st;
-
-        FastScanner() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreTokens()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
         }
     }
 }
