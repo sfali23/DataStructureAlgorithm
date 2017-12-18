9130ae932240c793e7d1eeeb2d8e7d6216f29065
sfali23 <f.syed.ali@gmail.com>
2017-12-17 20:58:54 -0500

    solution of IsBst and tests

--- DataStructures/Assignment4/src/main/scala/ValidateBinarySearchTree.scala
+++ DataStructures/Assignment4/src/main/scala/ValidateBinarySearchTree.scala
@@ -1,6 +1,104 @@
+import java.util.Scanner
+
 /**
   * @author sali
   */
-class ValidateBinarySearchTree {
+trait ValidateBinarySearchTreeWork {
 
+  class Node() {
+    private var _value: Long = 0L
+    def value: Long = _value
+    def value_=(v: Long): Unit = _value = v
+
+    private var _index: Int = 0
+    def index: Int = _index
+    def index_=(i: Int): Unit = _index = i
+
+    private var _left: Option[Node] = None
+    def left: Option[Node] = _left
+    def left_=(l: Node): Unit = _left = Some(l)
+
+    private var _right: Option[Node] = None
+    def right: Option[Node] = _right
+    def right_=(r: Node): Unit = _right = Some(r)
+
+    private var _parent: Option[Node] = None
+    def parent: Option[Node] = _parent
+    def parent_=(node: Node): Unit = _parent = Some(node)
+  }
+
+  private def readData(scanner: Scanner): Option[Node] = {
+    val numOfVertices = scanner.nextInt()
+    val tree = Array.ofDim[Node](numOfVertices)
+    for (i <- 0 until numOfVertices) {
+      var node = tree(i)
+      if (node == null) {
+        node = new Node()
+        tree(i) = node
+      }
+
+      node.value = scanner.nextLong()
+      node.index = i
+      val indexOfLeftChild = scanner.nextInt()
+      if (indexOfLeftChild > -1) {
+        var child = tree(indexOfLeftChild)
+        if (child == null) {
+          child = new Node()
+          child.index = indexOfLeftChild
+          tree(indexOfLeftChild) = child
+        }
+        node.left = child
+        child.parent = node
+      }
+
+      val indexOfRightChild = scanner.nextInt()
+      if (indexOfRightChild > -1) {
+        var child = tree(indexOfRightChild)
+        if (child == null) {
+          child = new Node()
+          child.index = indexOfRightChild
+          tree(indexOfRightChild) = child
+        }
+        node.right = child
+        child.parent = node
+      }
+
+    }
+    tree.headOption
+  }
+
+  def isBinarySearchTree(root: Option[Node]): Boolean = isBinarySearchTree(root, Long.MinValue, Long.MaxValue)
+
+  private def isBinarySearchTree(mayBeRoot: Option[Node], min: Long, max: Long): Boolean =
+    if (mayBeRoot.isEmpty) true
+    else {
+      val root = mayBeRoot.get
+      val value = root.value
+
+      (value > min && value < max) && //
+      isBinarySearchTree(root.left, min, value) && //
+      isBinarySearchTree(root.right, value, max)
+    }
+
+  private def runTest(testIndex: Int): Unit = {
+    val fileNamePrefix = f"$testIndex%02d"
+    System.setIn(ValidateBinarySearchTree.getClass.getResourceAsStream(s"/is-bst/$fileNamePrefix"))
+    solve()
+  }
+
+  def solve(): Unit = {
+    val scanner = new Scanner(System.in)
+    val runnable = new Runnable {
+      override def run(): Unit = {
+        val root = readData(scanner)
+        if (isBinarySearchTree(root)) println("CORRECT") else println("INCORRECT")
+      }
+    }
+
+    new Thread(null, runnable, "1", 1 << 26).start()
+  }
+}
+
+object ValidateBinarySearchTree extends App with ValidateBinarySearchTreeWork {
+  solve()
 }
